9abc7cced8b476ffba1cef77a10bc2443d9d7205
@caiwei <caiwei@shopee.com>
2024-02-22 15:26:48 +0800

    调整测试case

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,11 +2,10 @@ package main
 
 import "testing"
 
-func BenchmarkLiteralTest(b *testing.B) {
-	b.ResetTimer()
+func TestFeatures(t *testing.T) {
+	ips := []string{"10.12.78.221", "10.129.112.8", "10.129.112.11", "10.12.77.110", "10.12.77.111", "10.12.77.107"}
 
-	b.Run("fingerprint", func(b *testing.B) {
-		ips := []string{"10.12.78.221", "10.129.112.8", "10.129.112.11", "10.12.77.110", "10.12.77.111", "10.12.77.107"}
-		doScan(ips, "80,443,9990")
+	t.Run("fingerprint", func(t *testing.T) {
+		doScan(ips, "1-10000")
 	})
 }
