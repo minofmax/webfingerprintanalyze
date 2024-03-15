8b8437c37917ebebf5394bf444d26542433cb2db
@caiwei <caiwei@shopee.com>
2024-03-15 16:44:39 +0800

    更新test

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestFeatures(t *testing.T) {
-	ips := []string{"10.12.78.221", "10.129.112.8", "10.129.112.11", "10.12.77.110", "10.12.77.111", "10.12.77.107"}
+	ips := []string{}
 
 	t.Run("fingerprint", func(t *testing.T) {
-		doScan(ips, "1-10000")
+		doScan(ips, "80,443,9990")
 	})
 }
